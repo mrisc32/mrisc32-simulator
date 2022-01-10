53912b00ff735cbed16f7cd4a0d173c90a64f368
m <m@bitsnbites.eu>
2022-01-10 18:59:11 +0100

    elf32: Replace ELF32 defines with elf32_defs.hpp
    This should clear up any possible questions about GPL dependencies.

--- sim/elf32.cpp
+++ sim/elf32.cpp
@@ -23,6 +23,7 @@
 //--------------------------------------------------------------------------------------------------
 
 #include "elf32.hpp"
+#include "elf32_defs.hpp"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -56,59 +57,6 @@ int elf32_stat(const char* filename, Elf32Info* info) {
   info->max_address = 0;
   return elf32_parse(filename, info);
 }
-
-// Borrowed from /usr/include/elf.h of a Linux system.
-
-typedef uint16_t Elf32_Half;      ///< Type for a 16-bit quantity.
-typedef uint32_t Elf32_Word;      ///< Type for unsigned 32-bit quantities.
-typedef int32_t Elf32_Sword;      ///< Type for signed 32-bit quantities.
-typedef uint64_t Elf32_Xword;     ///< Type for unsigned 64-bit quantities.
-typedef int64_t Elf32_Sxword;     ///< Type for signed 64-bit quantities.
-typedef uint32_t Elf32_Addr;      ///< Type of addresses.
-typedef uint32_t Elf32_Off;       ///< Type of file offsets.
-typedef uint16_t Elf32_Section;   ///< Type for section indices, which are 16-bit quantities.
-typedef Elf32_Half Elf32_Versym;  ///< Type for version symbol information.
-
-#define EI_NIDENT (16)
-typedef struct {
-  unsigned char e_ident[EI_NIDENT];  ///< Magic number and other info.
-  Elf32_Half e_type;                 ///< Object file type.
-  Elf32_Half e_machine;              ///< Architecture.
-  Elf32_Word e_version;              ///< Object file version.
-  Elf32_Addr e_entry;                ///< Entry point virtual address.
-  Elf32_Off e_phoff;                 ///< Program header table file offset.
-  Elf32_Off e_shoff;                 ///< Section header table file offset.
-  Elf32_Word e_flags;                ///< Processor-specific flags.
-  Elf32_Half e_ehsize;               ///< ELF header size in bytes.
-  Elf32_Half e_phentsize;            ///< Program header table entry size.
-  Elf32_Half e_phnum;                ///< Program header table entry count.
-  Elf32_Half e_shentsize;            ///< Section header table entry size.
-  Elf32_Half e_shnum;                ///< Section header table entry count.
-  Elf32_Half e_shstrndx;             ///< Section header string table index.
-} Elf32_Ehdr;
-
-typedef struct {
-  Elf32_Word sh_name;       ///< Section name (string tbl index).
-  Elf32_Word sh_type;       ///< Section type.
-  Elf32_Word sh_flags;      ///< Section flags.
-  Elf32_Addr sh_addr;       ///< Section virtual addr at execution.
-  Elf32_Off sh_offset;      ///< Section file offset.
-  Elf32_Word sh_size;       ///< Section size in bytes.
-  Elf32_Word sh_link;       ///< Link to another section.
-  Elf32_Word sh_info;       ///< Additional section information.
-  Elf32_Word sh_addralign;  ///< Section alignment.
-  Elf32_Word sh_entsize;    ///< Entry size if section holds table.
-} Elf32_Shdr;
-
-// Section header type
-#define SHT_NULL 0         ///< Section header table entry unused.
-#define SHT_PROGBITS 1     ///< Program data.
-#define SHT_NOBITS 8       ///< Program space with no data (bss).
-#define SHT_INIT_ARRAY 14  ///< Array of constructors.
-#define SHT_FINI_ARRAY 15  ///< Array of destructors.
-
-// Section header flags.
-#define SHF_ALLOC (1 << 1)  ///< Occupies memory during execution.
 
 int elf32_parse(const char* filename, Elf32Info* info) {
   Elf32_Ehdr elf_header;
