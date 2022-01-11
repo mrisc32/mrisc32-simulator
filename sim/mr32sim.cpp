d7f4162f608d7fa6371d4b4b062f627665112d67
m <m@bitsnbites.eu>
2022-01-11 19:14:44 +0100

    sim: Increase MC1-announced VRAM size to 512 KB

--- sim/mr32sim.cpp
+++ sim/mr32sim.cpp
@@ -607,7 +607,7 @@ int main(const int argc, const char** argv) {
     const uint32_t MMIO_START = 0xc0000000u;
     if (config_t::instance().ram_size() >= (MMIO_START + 64)) {
       ram.store32(MMIO_START + 8, 70000000);     // CPUCLK
-      ram.store32(MMIO_START + 12, 128 * 1024);  // VRAMSIZE
+      ram.store32(MMIO_START + 12, 512 * 1024);  // VRAMSIZE
       ram.store32(MMIO_START + 20, 1920);        // VIDWIDTH
       ram.store32(MMIO_START + 24, 1080);        // VIDHEIGHT
       ram.store32(MMIO_START + 28, 60 * 65536);  // VIDFPS
